dc633e832580dbf362122e482f4d235546fd97d1
mao15007124568 <3040125154@qq.com>
2019-03-25 17:40:40 +0800

    jdfkal;sfj

---
+++ my-project/src/pages/test/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+     <i-button type="primary" bind:click="handleClick">这是一个按钮</i-button>
+    </div>
+</template>
+
+<script>
+ Page({
+    data: {
+        current: 'homepage'
+    },
+
+    handleChange ({ detail }) {
+        this.setData({
+            current: detail.key
+        });
+    }
+});
+</script>
+
+<style>
+
+</style>
